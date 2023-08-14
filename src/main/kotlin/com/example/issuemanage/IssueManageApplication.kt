88e4a00d9bb0495b4c1e44dc52efd24f9862e649
mck0324 <dch3030@naver.com>
2023-08-15 01:02:24 +0900

    first commit

---
+++ src/main/kotlin/com/example/issuemanage/IssueManageApplication.kt
@@ -0,0 +1,11 @@
+package com.example.issuemanage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IssueManageApplication
+
+fun main(args: Array<String>) {
+	runApplication<IssueManageApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/issuemanage/IssueManageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.issuemanage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IssueManageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
