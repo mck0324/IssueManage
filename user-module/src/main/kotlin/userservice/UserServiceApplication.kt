70afec3b583a8b42c51de1aa8cdb7c5bf428311d
mck0324 <dch3030@naver.com>
2023-08-17 15:21:08 +0900

    R2DBC를 이용하여 DB작업

--- user-module/src/main/kotlin/userservice/UserServiceApplication.kt
+++ user-module/src/main/kotlin/userservice/UserServiceApplication.kt
@@ -1,7 +1,12 @@
 package userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class UserServiceApplication {
-}
+@ConfigurationPropertiesScan
+class UserServiceApplication
+    fun main(args: Array<String>) {
+        runApplication<UserServiceApplication>(*args)
+    }
