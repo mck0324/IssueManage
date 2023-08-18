05958b13f2f8c48e2ecf97f742a23e37eb38d790
mck0324 <dch3030@naver.com>
2023-08-18 10:54:32 +0900

    TEST)JWT토큰 인증(decode)

--- user-module/src/test/kotlin/userservice/utils/JWTUtilsTest.kt
+++ user-module/src/test/kotlin/userservice/utils/JWTUtilsTest.kt
@@ -1,6 +1,8 @@
 package userservice.utils
 
+import com.auth0.jwt.interfaces.DecodedJWT
 import mu.KotlinLogging
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import userservice.config.JWTProperties
@@ -28,5 +30,39 @@ class JWTUtilsTest {
         assertNotNull(token)
 
         logger.info { "token: $token" }
+    }
+
+    @Test
+    fun decodeTest() {
+        val jwtClaim = JWTClaim(
+                userId = 1,
+                email = "gigi@naver.com",
+                profileUrl = "gigi.jpg",
+                username = "gigi"
+        )
+        val properties = JWTProperties(
+                issuer = "mm",
+                subject = "auth",
+                expiresTime = 3600,
+                secret = "my-secret"
+        )
+        val token = JWTUtils.createToken(jwtClaim,properties)
+        val decode: DecodedJWT = JWTUtils.decode(token, secret = properties.secret, issuer = properties.issuer )
+
+        with(decode) {
+            logger.info { "claims : $claims" }
+
+            val userId = claims["userId"]!!.asLong()
+            assertEquals(userId, jwtClaim.userId)
+
+            val email = claims["email"]!!.asString()
+            assertEquals(email, jwtClaim.email)
+
+            val profileUrl = claims["profileUrl"]!!.asString()
+            assertEquals(profileUrl, jwtClaim.profileUrl)
+
+            val username = claims["username"]!!.asString()
+            assertEquals(username, jwtClaim.username)
+        }
     }
 }
